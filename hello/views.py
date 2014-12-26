f610e532befc12efeba9dd2349f30f8a66804780
Sam <s.ratliff13@gmail.com>
2014-12-26 15:14:51 -0500

    Sam Test Commit2

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    return HttpResponse('Sam Test Greeting!')
 
 
 def db(request):
